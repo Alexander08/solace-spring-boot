cc11f4b69c434b6efebec780ea3b7261bac78ec6
Alexander08 <oprescu.alexandru08@gmail.com>
2023-05-01 19:50:34 +0200

    fix failing test

--- producer/src/test/java/com/oprescu/solace/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/oprescu/solace/producer/ProducerApplicationTests.java
@@ -1,9 +1,16 @@
 package com.oprescu.solace.producer;
 
+import com.solacesystems.jcsmp.JCSMPSession;
+import com.solacesystems.jcsmp.XMLMessageProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
+@MockBean({
+    JCSMPSession.class,
+    XMLMessageProducer.class
+})
 class ProducerApplicationTests {
 
   @Test
